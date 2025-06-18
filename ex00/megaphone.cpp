2e68797c225565af751eaf1c64b149d0e8f8eb3d
Giselle Maccha Borja <gisellemaccha27@gmail.com>
2025-06-18 11:10:26 +0200

    Add files via upload

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmaccha- <gmaccha-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 10:16:21 by gmaccha-          #+#    #+#             */
+/*   Updated: 2025/06/18 11:04:47 by gmaccha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main (int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (i < argc)
+		{
+			str = argv[i];
+			j = 0;
+			while (str[j])
+			{
+				std::cout << (char)std::toupper(str[j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
