6740b1eb924b87c235f03efecfbfae64735d792b
Giselle Maccha Borja <gisellemaccha27@gmail.com>
2025-06-20 10:15:47 +0200

    Add files via upload
    update

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gmaccha- <gmaccha-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 13:01:12 by gmaccha-          #+#    #+#             */
-/*   Updated: 2025/06/19 13:35:23 by gmaccha-         ###   ########.fr       */
+/*   Updated: 2025/06/20 10:11:19 by gmaccha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,24 @@ using   namespace std;
 
 int main()
 {
-    PhoneBook phonebook;
-    string cmd;
-    
-    while (true)
-    {
-        cout << "Input a command (ADD, SEARCH, EXIT): ";
-        getline(cin, cmd);
-        if (cmd == "ADD")
-            phonebook.addContact();
-        else if (cmd == "SEARCH")
-            phonebook.searchContact();
-        else if (cmd == "EXIT")
-        {
-            cout << "!See you later!" << endl;
-            break;
-        }
-        else
-            cout << "Unknown command." << endl;
-    }
-    return (0);
+	PhoneBook phonebook;
+	string cmd;
+	
+	while (true)
+	{
+		cout << "Input a command (ADD, SEARCH, EXIT): ";
+		getline(cin, cmd);
+		if (cmd == "ADD")
+			phonebook.addContact();
+		else if (cmd == "SEARCH")
+			phonebook.searchContact();
+		else if (cmd == "EXIT")
+		{
+			cout << "!See you later!" << endl;
+			break;
+		}
+		else
+			cout << "Unknown command." << endl;
+	}
+	return (0);
 }
